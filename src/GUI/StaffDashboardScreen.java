4bbe3426433fa764fbb8775f5e416083c26321b9
AhmadAlSadoon <aalsadoon1@sheffield.ac.uk>
2023-11-27 21:47:35 +0000

    Added most functionalities to staffDashboard saif will add functions and buttton logic for orderfulfillment etc.

--- src/GUI/StaffDashboardScreen.java
+++ src/GUI/StaffDashboardScreen.java
@@ -27,66 +27,28 @@ public class StaffDashboardScreen extends JFrame {
         JTable table = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(table);
         getContentPane().add(scrollPane, BorderLayout.CENTER);
-        readFromDatabase();
+        initializeUI();
     }
 
-    private void readFromDatabase() {
-        try {
-                    
-            String url = "jdbc:mysql://stusql.dcs.shef.ac.uk/team058";
-            String dbUsername = "team058";
-            String dbPassword = "eel7Ahsi0";
-            Connection connection = DriverManager.getConnection(url,dbUsername,dbPassword);
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Product");
 
-            // Retrieve column names
-            int columnCount = rs.getMetaData().getColumnCount();
-            ArrayList<String> columnNamesList = new ArrayList<>();
-            for (int i = 1; i <= columnCount; i++) {
-                columnNamesList.add(rs.getMetaData().getColumnName(i));
-            }
-            tableModel.setColumnIdentifiers(columnNamesList.toArray());
-
-            // Populate table with data
-            while (rs.next()) {
-                ArrayList<Object> rowDataList = new ArrayList<>();
-                for (int i = 1; i <= columnCount; i++) {
-                    rowDataList.add(rs.getObject(i));
-                }
-                tableModel.addRow(rowDataList.toArray());
-            }
-
-            rs.close();
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
- 
-    }
    
     private void initializeUI() {
-    mainPanel = new JPanel();
-    cardLayout = new CardLayout();
-    mainPanel.setLayout(cardLayout);
+        mainPanel = new JPanel();
+        cardLayout = new CardLayout();
+        mainPanel.setLayout(cardLayout);
 
-    // Add different panels here
-    // For example:
-    mainPanel.add(createOrdersPanel(), "Orders");
-    mainPanel.add(createInventoryPanel(), "Inventory");
-    mainPanel.add(createUserManagementPanel(), "UserManagement");
+        // Create different panels here
+        mainPanel.add(createOrdersPanel(), "Orders");
+        mainPanel.add(createInventoryPanel(), "Inventory");
 
-
-    // Add mainPanel to the JFrame
-    // Layout for the entire frame
-    setLayout(new BorderLayout());
-    add(createTopPanel(), BorderLayout.NORTH);
-    add(mainPanel, BorderLayout.CENTER);
-
-    // Add a sidebar or menu to switch views
-    add(createSidebar(), BorderLayout.WEST);
+        // Set layout for the entire frame
+        setLayout(new BorderLayout());
+        add(createTopPanel(), BorderLayout.NORTH);
+        add(mainPanel, BorderLayout.CENTER);
+        add(createSidebar(), BorderLayout.WEST);
     }
+
+
 
     private JPanel createTopPanel() {
         JPanel topPanel = new JPanel(new BorderLayout());
@@ -109,113 +71,110 @@ public class StaffDashboardScreen extends JFrame {
     }
 
     private JPanel createOrdersPanel() {
-    JPanel orderPanel = new JPanel(new BorderLayout());
+        JPanel ordersPanel = new JPanel(new BorderLayout());
 
-    // Table for pending orders
-    String[] columnNames = {"Order ID", "Date/Time", "Details"};
-    JTable ordersTable = new JTable(new Object[][]{}, columnNames);
-    orderPanel.add(new JScrollPane(ordersTable), BorderLayout.CENTER);
+        // Fetch data from the database
+        DefaultTableModel ordersModel = fetchDataFromDatabaseForOrders();
+    
+        // Create the table using the retrieved data
+        JTable ordersTable = new JTable(ordersModel);
+        JScrollPane scrollPane = new JScrollPane(ordersTable);
+        ordersPanel.add(scrollPane, BorderLayout.CENTER);
+    
+        // Buttons for order management
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        JButton fulfillButton = new JButton("Fulfill Order");
+        JButton deleteOrderButton = new JButton("Delete Order");
+        buttonPanel.add(fulfillButton);
+        buttonPanel.add(deleteOrderButton);
+    
+        fulfillButton.addActionListener(e -> {
+            int selectedRow = ordersTable.getSelectedRow();
+            if (selectedRow != -1) {
+                Object orderId = ordersTable.getValueAt(selectedRow, 0);
+                //for saif to add the functionality of the buttons
+            } 
+            else {
+                JOptionPane.showMessageDialog(null, "No order selected for to be fulfilled.");
+            }
+        });
+    
+        deleteOrderButton.addActionListener(e -> {
+            int selectedRow = ordersTable.getSelectedRow();
+            if (selectedRow != -1) {
+                Object orderId = ordersTable.getValueAt(selectedRow, 0);
+                //for saif to add the functionality of the buttons
+            } 
+            else {
+                JOptionPane.showMessageDialog(null, "No order selected for deletion.");
 
-    // Buttons for order management
-    JPanel buttonPanel = new JPanel(new FlowLayout());
-    JButton fulfillButton = new JButton("Fulfill Order");
-    JButton deleteOrderButton = new JButton("Delete Order");
-    buttonPanel.add(fulfillButton);
-    buttonPanel.add(deleteOrderButton);
-
-    // Add action listeners for buttons
-
-    orderPanel.add(buttonPanel, BorderLayout.SOUTH);
-
-    return orderPanel;
+            }
+        });
+    
+        // Add button panel to the ordersPanel
+        ordersPanel.add(buttonPanel, BorderLayout.SOUTH);
+    
+        return ordersPanel;
     }
 
-    private DefaultTableModel userModel;
-    private DefaultTableModel createUserModel() {
-        String[] userColumnNames = {"User ID", "Name", "Role"};
-        Object[][] userData = {
-            {"001", "John Doe", "Staff"},
-            {"002", "Jane Smith", "Manager"},
-            // More dummy rows
-            // dummy rows
-        };
-        userModel = new DefaultTableModel(userData, userColumnNames);
-        return userModel;
-    }
+    private DefaultTableModel fetchDataFromDatabaseForOrders() {
+        DefaultTableModel ordersModel = new DefaultTableModel();
+    
+        try {
+            // Establish database connection
+            String url = "jdbc:mysql://stusql.dcs.shef.ac.uk/team058";
+            String dbUsername = "team058";
+            String dbPassword = "eel7Ahsi0";
+            Connection connection = DriverManager.getConnection(url, dbUsername, dbPassword);
+            Statement stmt = connection.createStatement();
+    
+            // Modify the query to select specific columns
+            ResultSet rs = stmt.executeQuery("SELECT OrderLineID, Quantity, LineCost, ProductCode, Status FROM OrderLine");
+    
+            // Add columns to the table model
+            ordersModel.addColumn("Order ID");
+            ordersModel.addColumn("Quantity");
+            ordersModel.addColumn("Line Cost");
+            ordersModel.addColumn("Product Code");
+            ordersModel.addColumn("Status");
 
-    private JPanel createUserManagementPanel() {
-    JPanel userManagementPanel = new JPanel(new BorderLayout());
-
-    // Search Panel
-    JPanel searchPanel = new JPanel();
-    searchPanel.add(new JLabel("Search User:"));
-    JTextField searchField = new JTextField(20);
-    JButton searchButton = new JButton("Search");
-    searchPanel.add(searchField);
-    searchPanel.add(searchButton);
-
-   // User Table with the model
-   JTable userTable = new JTable(createUserModel());
-   JScrollPane userScrollPane = new JScrollPane(userTable);
-
-    // Search Button Action Listener
-    searchButton.addActionListener(e -> {
-        String searchText = searchField.getText().toLowerCase();
-        TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(userModel);
-        userTable.setRowSorter(sorter);
-
-        if (searchText.trim().length() == 0) {
-            sorter.setRowFilter(null);
-        } else {
-            // Filter based on the second column (Name)
-            sorter.setRowFilter(RowFilter.regexFilter("(?i)" + searchText, 1));
+    
+            // Populate table with data
+            while (rs.next()) {
+                Object[] rowData = {
+                    rs.getInt("OrderLineID"),
+                    rs.getInt("Quantity"),
+                    rs.getDouble("Quantity"),
+                    rs.getString("ProductCode"),
+                    rs.getString("Status")
+                };
+                ordersModel.addRow(rowData);
+            }
+    
+            rs.close();
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    });
+    
+        return ordersModel;
+    }
 
-    // Action Buttons
-    JPanel actionPanel = new JPanel();
-    JButton promoteButton = new JButton("Promote to Manager");
-    JButton demoteButton = new JButton("Demote to Staff");
-    actionPanel.add(promoteButton);
-    actionPanel.add(demoteButton);
 
-    // Add components to the panel
-    userManagementPanel.add(searchPanel, BorderLayout.NORTH);
-    userManagementPanel.add(userScrollPane, BorderLayout.CENTER);
-    userManagementPanel.add(actionPanel, BorderLayout.SOUTH);
-
-    JButton userManagerButton = new JButton("User Management");
-    userManagerButton.addActionListener(e -> cardLayout.show(mainPanel, "UserManagement"));
-
-    return userManagementPanel;
-    }   
 
 
     private JPanel createInventoryPanel() {
-        JPanel inventoryPanel = new JPanel();
-        inventoryPanel.setLayout(new BorderLayout());
+        JPanel inventoryPanel = new JPanel(new BorderLayout());
 
-        // Dropdown for product categories
-        JComboBox<String> categoryDropdown = new JComboBox<>(new String[]{"Category 1", "Category 2", "Category 3"});
-        inventoryPanel.add(categoryDropdown, BorderLayout.NORTH);
-
-        // Dummy data for the table
-        String[] columnNames = {"Product ID", "Product Name", "Quantity"};
-        Object[][] data = {
-        {"001", "Locomotive A", "5"},
-        {"002", "Carriage B", "10"},
-        // More dummy rows
-        };
-
-
-        DefaultTableModel inventoryModel = new DefaultTableModel(data, columnNames);
+        // Fetch data from the database
+        DefaultTableModel inventoryModel = fetchDataFromDatabaseForInventory();
+    
+        // Create the table using the retrieved data
         JTable inventoryTable = new JTable(inventoryModel);
         JScrollPane scrollPane = new JScrollPane(inventoryTable);
         inventoryPanel.add(scrollPane, BorderLayout.CENTER);
 
-
-        // Add buttons for inventory management
-        // Buttons for inventory management
         JPanel buttonPanel = new JPanel();
         JButton addButton = new JButton("Add New Item");
         JButton editButton = new JButton("Edit Selected Item");
@@ -224,15 +183,80 @@ public class StaffDashboardScreen extends JFrame {
         buttonPanel.add(editButton);
         buttonPanel.add(deleteButton);
 
-        // Add action listeners for buttons
+        addButton.addActionListener(e -> {
 
+        });
+    
+        editButton.addActionListener(e -> {
+            int selectedRow = inventoryTable.getSelectedRow();
+            if (selectedRow != -1) {
+                Object itemId = inventoryTable.getValueAt(selectedRow, 0);
+
+            } 
+            else {
+                JOptionPane.showMessageDialog(null, "No order selected for deletion.");
+            }
+        });
+    
+        deleteButton.addActionListener(e -> {
+            int selectedRow = inventoryTable.getSelectedRow();
+            if (selectedRow != -1) {
+                Object itemId = inventoryTable.getValueAt(selectedRow, 0);
+
+            } 
+            else {
+                JOptionPane.showMessageDialog(null, "No order selected for deletion.");
+            }
+        });
+    
+        // Add button panel to the inventoryPanel
         inventoryPanel.add(buttonPanel, BorderLayout.SOUTH);
-
-        // Add sorting and searching
-        TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(inventoryModel);
-        inventoryTable.setRowSorter(sorter);
-
+    
         return inventoryPanel;
+    }
+
+    private DefaultTableModel fetchDataFromDatabaseForInventory() {
+        DefaultTableModel inventoryModel = new DefaultTableModel();
+    
+
+        try {
+            // Establish database connection
+            String url = "jdbc:mysql://stusql.dcs.shef.ac.uk/team058";
+            String dbUsername = "team058";
+            String dbPassword = "eel7Ahsi0";
+            Connection connection = DriverManager.getConnection(url, dbUsername, dbPassword);
+            Statement stmt = connection.createStatement();
+    
+            // Modify the query to select specific columns
+            ResultSet rs = stmt.executeQuery("SELECT ProductCode, BrandName, ProductName, FeatureCode, Stock FROM Product");
+    
+            // Add columns to the table model
+            inventoryModel.addColumn("Product Code");
+            inventoryModel.addColumn("Brand Name");
+            inventoryModel.addColumn("Product Name");
+            inventoryModel.addColumn("Feature Code");
+            inventoryModel.addColumn("Stock");
+    
+            // Populate table with data
+            while (rs.next()) {
+                Object[] rowData = {
+                    rs.getString("ProductCode"),
+                    rs.getString("BrandName"),
+                    rs.getString("FeatureCode"),
+                    rs.getString("ProductName"),
+                    rs.getInt("Stock")
+                };
+                inventoryModel.addRow(rowData);
+            }
+    
+            rs.close();
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+        return inventoryModel;
     }
 
 
@@ -244,17 +268,14 @@ public class StaffDashboardScreen extends JFrame {
         // Create buttons for the sidebar
         JButton ordersButton = new JButton("Orders");
         JButton inventoryButton = new JButton("Inventory");
-        JButton userManagerButton = new JButton("User Management");
 
         // Add action listeners to buttons
         ordersButton.addActionListener(e -> cardLayout.show(mainPanel, "Orders"));
         inventoryButton.addActionListener(e -> cardLayout.show(mainPanel, "Inventory"));
-        userManagerButton.addActionListener(e -> cardLayout.show(mainPanel, "UserManagement"));
 
         // Add buttons to the sidebar
         sidebar.add(ordersButton);
         sidebar.add(inventoryButton);
-        sidebar.add(userManagerButton);
 
         return sidebar;
     }
