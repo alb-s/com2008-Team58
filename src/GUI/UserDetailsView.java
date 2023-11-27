a3e5c94b0aa6db3aa5cdb26e4436b858ae1226c3
talhahazlan <125682993+talhahazlan@users.noreply.github.com>
2023-11-27 16:06:22 +0000

    create the function for update user details

--- src/GUI/UserDetailsView.java
+++ src/GUI/UserDetailsView.java
@@ -3,8 +3,20 @@ package GUI;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class UserDetailsView extends JFrame {
+    private JTextField oldEmailField = new JTextField(15), 
+                       newEmailField = new JTextField(15), 
+                       forenameField = new JTextField(15), 
+                       surnameField = new JTextField(15),
+                       houseNumberField = new JTextField(15), 
+                       roadNameField = new JTextField(15), 
+                       cityNameField = new JTextField(15), 
+                       postcodeField = new JTextField(15);
 
     public UserDetailsView() {
         setTitle("Edit Details");
@@ -37,10 +49,22 @@ public class UserDetailsView extends JFrame {
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(4, 4, 4, 4);
-
+    
         for (String field : fields) {
             sectionPanel.add(new JLabel(field + ":"), gbc);
-            sectionPanel.add(new JTextField(15), gbc);
+            JTextField textField = new JTextField(15);
+            sectionPanel.add(textField, gbc); // Correctly add the initialized textField
+    
+            switch (field) {
+                case "Old Email": oldEmailField = textField; break;
+                case "New Email": newEmailField = textField; break;
+                case "Forename": forenameField = textField; break;
+                case "Surname": surnameField = textField; break;
+                case "House Number": houseNumberField = textField; break;
+                case "Road Name": roadNameField = textField; break;
+                case "City Name": cityNameField = textField; break;
+                case "Postcode": postcodeField = textField; break;
+            }
         }
 
         return sectionPanel;
@@ -54,23 +78,59 @@ public class UserDetailsView extends JFrame {
         
         editBankDetailsButton.addActionListener((ActionEvent e) -> {
         // Redirect to edit bank details Screen
+        new EditBankDetails().setVisible(true);
         });
    
         saveButton.addActionListener((ActionEvent e) -> {
             // Save button logic
-            JOptionPane.showMessageDialog(this, "Details saved successfully.");
+            saveUserDetails();
         });
 
         cancelButton.addActionListener((ActionEvent e) -> {
-         // Close the frame
+            UserDetailsView.this.dispose(); // Close the frame
         });
-
+        
         controlPanel.add(saveButton);
         controlPanel.add(cancelButton);
         controlPanel.add(editBankDetailsButton);
         return controlPanel;
     }
 
+    private void saveUserDetails() {
+        try {
+            // Establish Database Connection
+            Connection connection = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team058", "team058", "eel7Ahsi0");
+    
+            // SQL query with correct order of fields
+            String sql = "UPDATE Users SET email = ?, forename = ?, surname = ?, housenumber = ?, roadname = ?, cityname = ?, postcode = ? WHERE email = ?";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+    
+            // Set parameters from text fields
+            pstmt.setString(1, newEmailField.getText()); // New Email
+            pstmt.setString(2, forenameField.getText());
+            pstmt.setString(3, surnameField.getText());
+            pstmt.setString(4, houseNumberField.getText());
+            pstmt.setString(5, roadNameField.getText());
+            pstmt.setString(6, cityNameField.getText());
+            pstmt.setString(7, postcodeField.getText());
+            pstmt.setString(8, oldEmailField.getText()); // Old Email
+    
+            // Execute Update
+            int affectedRows = pstmt.executeUpdate();
+    
+            if (affectedRows > 0) {
+                JOptionPane.showMessageDialog(this, "Details updated successfully.");
+            } else {
+                JOptionPane.showMessageDialog(this, "No details updated. Check the old email.");
+            }
+    
+            pstmt.close();
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Error updating details: " + ex.getMessage());
+        }
+    }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new UserDetailsView().setVisible(true);
